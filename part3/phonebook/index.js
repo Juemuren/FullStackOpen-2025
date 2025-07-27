23694feecc0d941fb07ec05e7e641059acdf5aa3
Raind <juemuren1976@gmail.com>
2025-07-27 16:54:34 +0800

    3.11 phonebook full stack

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,6 +9,7 @@ morgan.token('content', (request) => {
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
