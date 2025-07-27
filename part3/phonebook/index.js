f94c9e8c385c4c385bae5514c425b5811ea38ae5
Raind <juemuren1976@gmail.com>
2025-07-27 16:28:32 +0800

    3.9 phonebook backend step9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('content', (request) => {
   return JSON.stringify(request.body)
 })
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
