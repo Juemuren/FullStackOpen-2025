cbd36deef5b096cddcf1708485e0a4503635a917
Raind <juemuren1976@gmail.com>
2025-07-28 18:56:40 +0800

    3.13: Phonebook database, step 1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,42 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
 
 morgan.token('content', (request) => {
   return JSON.stringify(request.body)
 })
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+app.use(express.static('dist'))
+app.use(express.json())
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
+let persons = []
 
 app.get('/', (request, response) => {
-  response.send('OK')
+  response.send('<p>OK</p>')
 })
 
 app.get('/info', (request, response) => {
@@ -46,17 +26,15 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -99,7 +77,13 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
