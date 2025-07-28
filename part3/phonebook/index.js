70cefde1719611a83c5f50b25e5ee5992fc7d42c
Raind <juemuren1976@gmail.com>
2025-07-29 01:47:56 +0800

    3.18*: Phonebook database step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -13,25 +13,35 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let persons = []
-
-// Need FIX
-app.get('/info', (request, response) => {
-  const peopleInfo = `<p>Phonebook has info for ${persons.length} people</p>`
-  const dateInfo = `<p>${new Date()}</p>`
-  response.send(peopleInfo + dateInfo)
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+        `)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
