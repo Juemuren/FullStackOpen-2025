56ace55f555255c427f10dedb6d12f4a2c7350d1
Raind <juemuren1976@gmail.com>
2025-08-01 20:53:18 +0800

    4.15: Blog List Expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,15 +2,16 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 
+const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
-logger.info('connecting to', config.MONGODB_URI)
-
 const mongoUrl = config.MONGODB_URI
+logger.info('connecting to', mongoUrl)
 mongoose.connect(mongoUrl)
   .then(() => {
     logger.info('connected to MongoDB')
@@ -23,5 +24,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
