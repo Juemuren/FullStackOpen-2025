8ecb75a5758a15fc103ca005cb8ec4e845e6b056
Raind <juemuren1976@gmail.com>
2025-08-01 21:13:53 +0800

    4.16*: Blog List Expansion, step4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
 const mongoose = require('mongoose')
@@ -25,5 +26,8 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -12,6 +12,11 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (password.length < 3) {
+    response.status(400).json({ error: 'password is too short' })
+    return
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
