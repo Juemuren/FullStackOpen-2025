bdd33adc8fa99088c5b422e31a994753d6a19df5
Raind <juemuren1976@gmail.com>
2025-08-12 23:44:20 +0800

    5.13: Blog List Tests, step 1

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -35,7 +35,7 @@ const Blog = ({ user, blog, updateBlog, deleteBlog }) => {
   }
 
   return (
-    <div style={blogStyle}>
+    <div style={blogStyle} className="blog">
       <div>
         {blog.title} {blog.author}
         <button onClick={changeShow}>{buttonLable}</button>
