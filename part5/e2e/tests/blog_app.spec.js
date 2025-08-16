10ad94647c4b52cd76a9bcce62913596c8314a2c
Raind <127611616+Juemuren@users.noreply.github.com>
2025-08-16 19:23:41 +0800

    5.18: Blog List End To End Testing, step 2

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,12 +1,34 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/tests/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Li TianSuo',
+        username: '114514',
+        password: '1919180'
+      }
+    })
+
     await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
-    await expect(page.getByTestId("username")).toBeVisible()
-    await expect(page.getByTestId("password")).toBeVisible()
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, '114514', '1919180')
+      await expect(page.getByText('Li TianSuo logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, '114514', 'wrong')
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
   })
 })
