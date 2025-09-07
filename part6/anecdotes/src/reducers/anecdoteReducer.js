d36c9416a1e44ba3c5f6a3a6ef2d1d9f7333cc50
Raind <127611616+Juemuren@users.noreply.github.com>
2025-09-07 22:23:05 +0800

    6.3: Anecdotes, step 1

--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,28 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const votedAnecdote = { 
+        ...anecdoteToVote, 
+        votes: anecdoteToVote.votes + 1 
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      )
+    }
+    default:
+      return state
+  }
+}
+
+export const voteFor = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
