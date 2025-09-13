2c6c878f0e014f12a6da2a4c08c79a8679b6bb99
Raind <127611616+Juemuren@users.noreply.github.com>
2025-09-13 11:14:15 +0800

    7.4: Anecdotes and Hooks step 1

--- part7/routed-anecdotes/src/components/AnecdoteForm.jsx
+++ part7/routed-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useField } from "../hooks"
 
 const AnecdoteForm = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -23,15 +22,15 @@ const AnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
