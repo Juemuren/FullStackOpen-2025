ffb2b3b1ff8b4d4c9d1ccb932be3b058cd0081f0
Raind <127611616+Juemuren@users.noreply.github.com>
2025-09-21 13:07:41 +0800

    8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -89,10 +89,18 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+
   type Query {
     dummy: Int
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -101,6 +109,7 @@ const resolvers = {
     dummy: () => 0,
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
