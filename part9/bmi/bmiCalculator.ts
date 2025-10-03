b6d69cddb3bb476828b7d685f676eac2b9689ee8
Raind <127611616+Juemuren@users.noreply.github.com>
2025-10-04 01:34:31 +0800

    9.3 Command line

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -2,16 +2,41 @@ const calculateBmi = (height: number, weight: number): string => {
     const bmi = 10000 * weight / (height * height)
     if (bmi < 18.5) {
         return "Underweight"
-    }
-    else if (bmi < 24.9) {
+    } else if (bmi < 24.9) {
         return "Normal range"
-    }
-    else if (bmi < 29.9) {
+    } else if (bmi < 29.9) {
         return "Overweight"
-    }
-    else {
+    } else {
         return "Obese"
     }
 }
 
-console.log(calculateBmi(180, 74))
+interface args {
+    height: number,
+    weight: number
+}
+
+export const parseArguments = (args: string[]): args => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+    if (args.length > 4) throw new Error('Too many arguments')
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3]),
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -1,10 +1,17 @@
-const TARGET = 2
+interface result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number,
+}
 
-const calculateExercises = (times: number[]): object => {
+const calculateExercises = (target: number, times: number[]): result => {
     const periodLength = times.length
     let trainingDays = 0
     let count = 0
-    const target = TARGET
 
     for (var i = 0; i < periodLength; i++) {
         if (times[i] > 0) {
@@ -45,4 +52,33 @@ const calculateExercises = (times: number[]): object => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]))
+interface args {
+    target: number,
+    times: number[]
+}
+
+const parseArguments = (args: string[]): args => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+
+    const numbers = args.slice(2).map(Number)
+
+    if (numbers.every(num => !isNaN(num))) {
+        return {
+            target: numbers[0],
+            times: numbers.slice(1)
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
+try {
+    const { target, times } = parseArguments(process.argv);
+    console.log(calculateExercises(target, times));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
