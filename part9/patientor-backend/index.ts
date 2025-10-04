278d28cfaa78629b90fc348e272c1af24da2132d
Raind <127611616+Juemuren@users.noreply.github.com>
2025-10-04 23:33:11 +0800

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/ping', (_req, res) => {
@@ -7,7 +10,7 @@ app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
