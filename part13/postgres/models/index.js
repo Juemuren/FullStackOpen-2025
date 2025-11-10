3034e222270319d9d1cfda56ba43ef5c8ca12fe8
Raind <127611616+Juemuren@users.noreply.github.com>
2025-11-11 02:23:34 +0800

    13.17

--- part13/postgres/models/index.js
+++ part13/postgres/models/index.js
@@ -3,10 +3,6 @@ const User = require('./user')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
-// Blog.sync({ alter: true })
-// User.sync({ alter: true })
-Blog.sync()
-User.sync()
 
 module.exports = {
   Blog, User
