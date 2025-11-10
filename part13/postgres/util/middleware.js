06057bce2aef6f3df8f0fdccaa81f4a6dcf089ba
Raind <127611616+Juemuren@users.noreply.github.com>
2025-11-10 23:54:06 +0800

    13.10

--- part13/postgres/util/middleware.js
+++ part13/postgres/util/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('./config')
+
 const errorHandle = (error, req, res, next) => {
   console.error(error)
 
@@ -11,6 +14,21 @@ const errorHandle = (error, req, res, next) => {
   next(error)
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
 module.exports = {
-  errorHandle
+  errorHandle,
+  tokenExtractor
 }
