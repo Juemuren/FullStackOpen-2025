62673ede631519eedadec4b73953ad07d275f785
Raind <127611616+Juemuren@users.noreply.github.com>
2025-11-11 09:10:50 +0800

    13.19

--- part13/postgres/models/index.js
+++ part13/postgres/models/index.js
@@ -1,9 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+Blog.belongsToMany(User, { through: ReadingList, as: "read_by_users" })
+User.belongsToMany(Blog, { through: ReadingList, as: "blog_reading_list" })
+
 module.exports = {
-  Blog, User
+  Blog, User, ReadingList
 }
